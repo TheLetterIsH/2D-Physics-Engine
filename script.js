217c52cfb57293be16e56b48141844b301d6a3a4
Harsh Valaki <96899883+TheLetterIsH@users.noreply.github.com>
2023-11-22 22:32:42 +0530

    ✨ Added method to draw ball!

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+function drawBall(x, y, radius, fillColor) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.fillStyle = fillColor;
+    ctx.fill();
+}
+
+drawBall(100, 100, 20, "mediumseagreen");
